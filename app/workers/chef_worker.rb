599f39c0811b6b29c5615589ac93b7603cd3b5f9
Shawn Catanzarite <me@shawncatz.com>
2012-11-07 15:15:25 -0800

    bring in worker code from Rig Server

--- app/workers/chef_worker.rb
+++ app/workers/chef_worker.rb
@@ -3,19 +3,19 @@ class ChefWorker < BaseWorker
 
   class << self
     def perform
-      #list = Rig::Plugin::Chef.role_list
-      #logger.info "#{self.name} found #{list.count} roles"
-      #list.each do |e|
-      #  name = e["name"]
-      #  d = {
-      #      description: e["description"],
-      #      internal: ! e["description"].match(/^internal/i).nil?
-      #  }
-      #  r = Role.where(name: name).first || Role.create(name: name)
-      #  r.update_attributes(d)
-      #  r
-      #end
-      #logger.info "#{self.name} complete"
+      list = Rig::Plugin::Chef.role_list
+      logger.info "#{self.name} found #{list.count} roles"
+      list.each do |e|
+        name = e["name"]
+        d = {
+            description: e["description"],
+            internal: ! e["description"].match(/^internal/i).nil?
+        }
+        r = Role.where(name: name).first || Role.create(name: name)
+        r.update_attributes(d)
+        r
+      end
+      logger.info "#{self.name} complete"
     end
   end
 end
--- app/workers/record_worker.rb
+++ app/workers/record_worker.rb
@@ -3,23 +3,23 @@ class RecordWorker < BaseWorker
 
   class << self
     def perform_create(options)
-      #id = options["id"]
-      #record = Record.find(id)
-      #raise "could not find record #{id}" unless record
-      #
-      #Rig::Model::Dns.create(record.name, record.values.first, :ttl => record.ttl)
-      #record.rid = record.name
-      #record.synced_at = Time.now
-      #record.save
+      id = options["id"]
+      record = Record.find(id)
+      raise "could not find record #{id}" unless record
+
+      Rig::Model::Dns.create(record.name, record.values.first, :ttl => record.ttl)
+      record.rid = record.name
+      record.synced_at = Time.now
+      record.save
     end
 
     def perform_destroy(options)
-      #id = options["id"]
-      #record = Record.unscoped.find(id)
-      #raise "could not find record #{id}" unless record
-      #
-      #Rig::Model::Dns.destroy(record.name)
-      #record.destroy
+      id = options["id"]
+      record = Record.unscoped.find(id)
+      raise "could not find record #{id}" unless record
+
+      Rig::Model::Dns.destroy(record.name)
+      record.destroy
     end
   end
 end
