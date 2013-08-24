3d01e8c8a578c5019fe31a497b9b39d1e5daaea2
Shawn Catanzarite <me@shawncatz.com>
2013-08-24 14:14:41 -0700

    qujo worker implementation

--- app/models/common/common_worker.rb
+++ app/models/common/common_worker.rb
@@ -2,37 +2,41 @@ module CommonWorker
   extend ActiveSupport::Concern
 
   included do
-    #def enqueue(action, options={})
-    #  w = "#{self.class.name}Worker".constantize
-    #  o = {"id" => self.id.to_s}.merge(options)
-    #  if Mystro.config.workers
-    #    logger.info "  ENQUEUE: #{w} #{action}"
-    #    Resque.enqueue(w, action, o) unless Rails.env.test?
-    #  else
-    #    logger.info "  RUNNING: #{w} #{action}"
-    #    w.perform(action, o)
-    #  end
-    #end
-
-    def enqueue(action, options={})
-      n = "Jobs::#{self.class.name.capitalize}::#{action.capitalize}"
-      c = n.constantize
-      d = {id: self.id.to_s, class: self.class.name}.merge(options)
-      j = c.create(data: d)
-      if Mystro.config.workers
-        logger.info "** ENQUEUE: JOB=#{j.id} NAME=#{n} DATA=#{d}"
-        j.enqueue
-      else
-        logger.info "** RUNNING: JOB=#{j.id} NAME=#{n} DATA=#{d}"
-        j.work
-      end
-      j.id
-    end
-
-    extend ClassMethods
+    include Qujo::Concerns::Model
   end
 
-  module ClassMethods
-
-  end
+  #included do
+  #  #def enqueue(action, options={})
+  #  #  w = "#{self.class.name}Worker".constantize
+  #  #  o = {"id" => self.id.to_s}.merge(options)
+  #  #  if Mystro.config.workers
+  #  #    logger.info "  ENQUEUE: #{w} #{action}"
+  #  #    Resque.enqueue(w, action, o) unless Rails.env.test?
+  #  #  else
+  #  #    logger.info "  RUNNING: #{w} #{action}"
+  #  #    w.perform(action, o)
+  #  #  end
+  #  #end
+  #
+  #  def enqueue(action, options={})
+  #    n = "Jobs::#{self.class.name.capitalize}::#{action.capitalize}"
+  #    c = n.constantize
+  #    d = {id: self.id.to_s, class: self.class.name}.merge(options)
+  #    j = c.create(data: d)
+  #    if Mystro.config.workers
+  #      logger.info "** ENQUEUE: JOB=#{j.id} NAME=#{n} DATA=#{d}"
+  #      j.enqueue
+  #    else
+  #      logger.info "** RUNNING: JOB=#{j.id} NAME=#{n} DATA=#{d}"
+  #      j.work
+  #    end
+  #    j.id
+  #  end
+  #
+  #  extend ClassMethods
+  #end
+  #
+  #module ClassMethods
+  #
+  #end
 end
