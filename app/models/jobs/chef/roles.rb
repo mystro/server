736210e6ce432315f15dc882c36acdd2b0673638
Shawn Catanzarite <me@shawncatz.com>
2013-12-18 13:19:35 -0800

    change to chef client, chef roles job is broken.

--- app/models/jobs/chef/roles.rb
+++ app/models/jobs/chef/roles.rb
@@ -3,12 +3,11 @@ class Jobs::Chef::Roles < Job
     if Mystro.config.plugins.chef && defined?(Mystro::Plugin::Chef)
       list = Mystro::Plugin::Chef.role_list
       info "found #{list.count} roles"
-      list.each do |e|
-        name = e["name"]
+      list.each do |name, url|
         info ".. #{name}"
         d = {
-            description: e["description"],
-            internal: ! e["description"].match(/^internal/i).nil?
+            description: url,
+            internal: false
         }
         r = Role.where(name: name).first || Role.create(name: name)
         r.update_attributes(d)
